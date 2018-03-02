190e0c00386cc10f6654df04fe92575bd9490e39
Tommi Siik <tsiika9@gmail.com>
2018-03-02 14:52:42 +0200

    Bootstrap
    Bootstrap added to react at this point. Material-UI is also installed as we change to it further on.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Bootstrap from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import Container from './components/Container';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container/>
+        
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
       </div>
     );
   }
