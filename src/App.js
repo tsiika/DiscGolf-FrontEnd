d497e62d1bf38129bc641d70d1e7f64d624a7f40
Tommi Siik <23232855+tsiika@users.noreply.github.com>
2018-03-05 17:32:19 +0200

    Header component andded
    It'll be removed or replaced since routing is going to be added in future.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Bootstrap from 'react-bootstrap';
-import logo from './logo.svg';
 import './App.css';
 import Container from './components/Container';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header />
         <Container/>
         
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
