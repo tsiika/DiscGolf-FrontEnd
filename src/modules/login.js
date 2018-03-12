990d51b0dbeacc8112142c83131a7c2b951e40e3
Tommi Siik <tsiika9@gmail.com>
2018-03-12 14:31:40 +0200

    Typo

--- src/modules/login.js
+++ src/modules/login.js
@@ -29,7 +29,7 @@ export default class LoginForm extends Component {
         <form>
           <h2>This is login page.</h2>
           Username: <br/>
-          <input type="text" name="username"/> <br/>
+          <input type="text" name="usrname"/> <br/>
           Password: <br/>
           <input type="password" name="pwd" /><br/>
           <input type="submit" name="Submit" />
