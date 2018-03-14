6cf12a01a3a59530f559f508a9f300c528a15f6e
Tommi Siik <tsiika9@gmail.com>
2018-03-14 13:21:41 +0200

    Changes to package.json

--- src/modules/Confirm.js
+++ src/modules/Confirm.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import Dashboard from './Dashboard';
+
 class App extends Component {
   render() {
     return (
       <div>
           <h2>Your input has been registered.</h2>
-          <Link to="/"><button>OK</button></Link>
+          <Link to="/Dashboard"><button>OK</button></Link>
       </div>
     );
   }
