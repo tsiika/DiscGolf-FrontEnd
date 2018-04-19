637533b97ac0dfb6002769276956704799ce2674
Tommi Siik <tsiika9@gmail.com>
2018-04-19 12:03:13 +0300

    Modules and backend functionality
    - Added course selection
    - Adding a new course form
    - Dashboard statistics (WIP)

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import { d3 } from 'd3';
+import axios from 'axios';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const API = 'http://localhost:5000/api/v0/statistics/5ad72e0876e4056ff784106e';
+
+
 
 class gameStats extends Component {
+	constructor(props) {
+		super(props);
+		this.state={
+			list:[]
+		}
+    }
+    
+    componentDidMount() {
+        this.getCourses();
+    }
+
+    getCourses = () => {
+        let fetchObj = {
+            method: "GET",
+            mode: "cors",
+            headers: {"Content-Type":"application/json"}
+        }
+        fetch(API, fetchObj).then((response)=> {
+            if(response.ok) {
+                response.json()
+                    .then((data)=> {
+                        console.log(data);
+                        this.setState({
+                            list:data.data
+                        });
+                    });
+            }
+        }).catch((error) => {
+            console.log("Error:"+error);
+        })
+    }
+            
+    
+        
     render() {
         return (
             <div>
-                <h3>Statistic</h3>
-                <p>Here is some sweet statistics.</p>
+                <h1>Statistic</h1>
+                    <h3>Played courses</h3>
+                        
+                <RaisedButton label="Show more" primary={true}></RaisedButton> 
+
+                <div>
+                    <p list={this.state.list}/>
+                </div>
             </div>
         )
     }
--- src/modules/Dashboard.js
+++ src/modules/Dashboard.js
@@ -6,9 +6,9 @@ import PrevMatch from '../components/PrevMatch';
 import PrevTourn from '../components/PrevTourn';
 import NewGame from '../components/NewGame';
 import GameStats from '../components/Statistic';
+import SelectPlayers from '../components/SelectPlayers'
 
 
- 
 class App extends Component {
   render() {
     return (
