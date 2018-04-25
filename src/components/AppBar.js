5e64c1394c71aa49dc0cc3232eb716e1030bd759
svihila <sakari.vihila@gmail.com>
2018-04-25 18:09:19 +0300

    Changed menu to show links for authenticated user only

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../api/Auth';
 
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
@@ -19,16 +20,23 @@ class AppBarC extends Component {
 
     handleToggle = () => this.setState({open: !this.state.open});
 
-render(){
-    /*
-    let logout = (Auth.isAuthenticated()) ?
-        <Link to="/logout" style={{ textDecoration: 'none' }}><MenuItem>Logout</MenuItem></Link> 
-        : 
-        '';
-    */
+    render() {
 
-    return(
-        <div>
+        // Links shown only for authenticated user
+        let links = ()=>{
+            if(Auth.isAuthenticated()) {
+                return (
+                    <div>
+                        <Link to="/dashboard" style={{ textDecoration: 'none' }}><MenuItem>Dashboard</MenuItem></Link>
+                        <Link to="/courses" style={{ textDecoration: 'none' }}><MenuItem>Courses</MenuItem></Link>
+                        <Link to="/logout" style={{ textDecoration: 'none' }}><MenuItem>Logout</MenuItem></Link>
+                    </div>
+                );
+            }
+        }
+
+        return(
+            <div>
                 <AppBar
                     iconClassNameRight="muidocs-icon-navigation-expand-more"
                     title="DiscGolf App"
@@ -41,21 +49,15 @@ render(){
                     onRequestChange={(open) => this.setState({open})}>
 
                     <AppBar title="Menu"/>
-                    <Link to="/" style={{ textDecoration: 'none' }}><MenuItem>Home</MenuItem></Link>
-                    <Link to="/register" style={{ textDecoration: 'none' }}><MenuItem>Register</MenuItem></Link>
-                    <Link to="/dashboard" style={{ textDecoration: 'none' }}><MenuItem>Dashboard</MenuItem></Link>
-                    <Link to="/courses" style={{ textDecoration: 'none' }}><MenuItem>Courses</MenuItem></Link>
-                    <Link to="/logout" style={{ textDecoration: 'none' }}><MenuItem>Logout</MenuItem></Link>
-                    <Link to="/admin" style={{ textDecoration: 'none' }}><MenuItem>Admin</MenuItem></Link>
 
+                    <Link to="/" style={{ textDecoration: 'none' }}><MenuItem>Home</MenuItem></Link>
+                    {links()}
                 </Drawer>
                 <Paper style={paperStyle} zDepth={5}>
-
                 </Paper>
-        </div>
-    );
-}
-
+            </div>
+        );
+    }
 }
 
 export default AppBarC;
