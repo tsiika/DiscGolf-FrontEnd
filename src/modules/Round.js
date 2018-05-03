dbd7337225d927a61dc219228af2a112f6298b74
svihila <sakari.vihila@gmail.com>
2018-05-03 13:26:50 +0300

    Building Round statistics view

--- src/modules/Round.js
+++ src/modules/Round.js
@@ -16,6 +16,7 @@ import {
     TableRowColumn
 } from 'material-ui/Table';
 import { RaisedButton } from 'material-ui';
+import { WSAESTALE } from 'constants';
 
 class Round extends Component {
 
@@ -25,8 +26,11 @@ class Round extends Component {
         this.getRoundData = this.getRoundData.bind(this);
         this.roundDataReceived = this.roundDataReceived.bind(this);
         this.roundDataFailure = this.roundDataFailure.bind(this);
+        this.makeRows = this.makeRows.bind(this);
 
-        this.state = { roundId: props.match.params.roundId };
+        this.state = { 
+            roundId: props.match.params.roundId 
+        };
     }
 
     componentDidMount() {
@@ -38,17 +42,58 @@ class Round extends Component {
     }
 
     roundDataReceived(round) {
-        console.log(round);
+        this.setState({round: round});
     }
 
     roundDataFailure(error) {
         console.error(error);
     }
 
+    makeRows() {
+
+        if(!this.state.round) return '';
+
+        let round = this.state.round;
+        let scores = round.scores;
+        let rows = [];
+        let ind = 0;
+
+        for(let playerId in scores) {
+            let playerScores = scores[playerId];
+            let columns = [];
+            columns.push( <TableRowColumn key={ind}>{playerScores.userName}</TableRowColumn> );
+
+            for(let i = 1; i <= round.course.fairways.length; i++) {
+                columns.push( <TableRowColumn key={i}>{playerScores[i].throwCount}</TableRowColumn> );
+            }
+
+            rows.push( <TableRow key={playerId}>{columns}</TableRow> );
+            ind++;
+        }
+
+        /* TODO : Display total throw counts etc... */
+
+        return rows;
+    }
+
+
     render() {
+
+        let tableRows = this.makeRows();
+
         return (
+            
             <div>
-                { this.state.roundId }
+                <Table selectable={false} multiSelectable={false}>
+                    <TableHeader displaySelectAll={false} enableSelectAll={false} adjustForCheckbox={false}>
+                        <TableRow>
+                            <TableHeaderColumn>Player</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody displayRowCheckbox={false}>
+                    {tableRows}
+                    </TableBody>
+                </Table>                
             </div>
         );
     }
