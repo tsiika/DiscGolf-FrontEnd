cdc317386f13a58de142e006c1fa91da21ae4d60
svihila <sakari.vihila@gmail.com>
2018-05-08 13:03:11 +0300

    Temporarily added logging of process.env

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+console.log('----- ENV  ------');
+console.log(process.env);
+
 render((
     <BrowserRouter>
         <MuiThemeProvider>
