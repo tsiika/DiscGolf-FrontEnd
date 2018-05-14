6c5ce3c20687662159b02d8404b277d92b5e9dea
Tommi Siik <tsiika9@gmail.com>
2018-05-14 14:26:38 +0300

    Chages to API 2

--- src/api/axiosAPI.js
+++ src/api/axiosAPI.js
@@ -1,4 +1,4 @@
-//const API = 'http://www.flatbread-api.herokuapp.com/api/v0/courses/';       /*Production*/
-const API = 'http://localhost:5000/api/v0/courses/';                        /*Development*/
+const API = 'http://www.flatbread-api.herokuapp.com/api/v0/courses/';       /*Production*/
+//const API = 'http://localhost:5000/api/v0/courses/';                        /*Development*/
 
 export default API;
